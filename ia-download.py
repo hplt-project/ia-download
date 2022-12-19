e02a7808aa8301ff86b3090a07e809038ce3f401
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-12-19 18:05:06 +0000

    Import ia-download.py

---
+++ ia-download.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import os
+import hashlib
+import csv
+import internetarchive as ia
+import random
+from datetime import datetime
+from typing import NamedTuple
+from multiprocessing import Pool
+from itertools import repeat
+
+
+class DownloadError(RuntimeError):
+	pass
+
+
+class File(NamedTuple):
+	name: str
+	url: str
+	md5: str
+
+
+class Download(NamedTuple):
+	path: str
+	size: int
+	md5: str
+	time: int
+
+
+def download_file(session, file:File, file_path:str) -> Download:
+	# Construct temporary filename in same directory
+	dest_dir, file_name = os.path.split(file_path)
+	temp_path = os.path.join(dest_dir, f'.{file_name}~{os.getpid()}')
+
+	start_time = datetime.now()
+
+	# Fetch header
+	response = session.get(file.url, stream=True, timeout=12, auth=ia.auth.S3Auth(session.access_key, session.secret_key))
+	if not response.ok:
+		raise DownloadError(response.reason)
+
+	# Fetch body, calculate checksum as we read through its chunks
+	digest = hashlib.md5()
+	size = 0
+	try:
+		with open(temp_path, 'wb') as fout:
+			for chunk in response.iter_content(chunk_size=1048576):
+				size += fout.write(chunk)
+				digest.update(chunk)
+
+		if digest.hexdigest() != file.md5:
+			raise DownloadError('md5 mismatch')
+
+		# Download finished and no errors! Move file to its permanent destination.
+		os.rename(temp_path, file_path)
+	finally:
+		# Clean up tempfile in case of error
+		# TODO: Resume download if we implement Partial or chunked download.
+		if os.path.exists(temp_path):
+			os.unlink(temp_path)
+
+	return Download(file_path, size, digest.hexdigest(), (datetime.now() - start_time).seconds)
+
+
+def worker_setup():
+	global session
+	session = ia.api.get_session()
+	session.mount_http_adapter(max_retries=2)
+
+
+def worker_download_file(entry):
+	global session
+	(item, file), dest_dir = entry
+	item_path = os.path.join(dest_dir, item)
+	file_path = os.path.join(item_path, file.name)
+
+	retval = None
+	if not os.path.exists(file_path):
+		try:
+			os.makedirs(item_path, exist_ok=True)
+			retval = download_file(session, file, file_path)
+		except Exception as err:
+			retval = err
+	
+	return item, file, retval
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--jobs', '-j', type=int, default=os.cpu_count(), help='parallel downloads')
+	parser.add_argument('--dest', '-d', type=str, default='.', help='destination directory')
+	parser.add_argument('--shuffle', action='store_true', help='download items in random order')
+	parser.add_argument('--filter', default='*.warc.gz', help='filename filter')
+	parser.add_argument('identifiers', type=str, nargs='*', help='IA identifiers to download warcs from. If none specified read from stdin')
+
+	args = parser.parse_args()
+
+	session = ia.api.get_session()
+
+	if not args.identifiers:
+		args.identifiers = (line.rstrip('\n') for line in sys.stdin)
+
+	if args.shuffle:
+		args.identifiers = list(args.identifiers)
+		random.shuffle(args.identifiers)
+
+	files = (
+		(item, File(file.name, file.url, file.md5))
+		for item in args.identifiers
+		for file in session.get_item(item).get_files(glob_pattern=args.filter)
+	)
+
+	# Keep a counter of how often downloads fail. If it keeps happening, stop
+	# because we might just making things worse.
+	consecutive_errors = 0
+
+	out = csv.DictWriter(sys.stdout, ['timestamp', 'item', 'name', 'path', 'size', 'time', 'md5', 'error'], delimiter='\t')
+
+	with Pool(args.jobs, initializer=worker_setup) as pool:
+		for item, file, retval in pool.imap_unordered(worker_download_file, zip(files, repeat(args.dest))):
+			if retval is None:
+				continue
+			elif isinstance(retval, Download):
+				out.writerow({
+					'timestamp': datetime.now().isoformat(),
+					'item': item,
+					'name': file.name,
+					'path': retval.path,
+					'size': retval.size,
+					'time': retval.time,
+					'md5': retval.md5,
+					
+				})
+				consecutive_errors = 0
+			else:
+				out.writerow({
+					'timestamp': datetime.now().isoformat(),
+					'item': item,
+					'name': file.name,
+					'error': str(retval)
+				})
+				consecutive_errors += 1
+
+			if consecutive_errors > 100:
+				raise RuntimeError('More than a 100 consecutive errors')
